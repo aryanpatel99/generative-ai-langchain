69b9fa10d4dafaa1235300dba70a6f05cbba1125
Aryan Patel <aryanpatel6215@gmail.com>
2026-02-19 23:54:37 +0530

    Fix CGPA field validation in Student model and add comment for age type in User TypedDict

--- structured_output/pydantic_demo.py
+++ structured_output/pydantic_demo.py
@@ -5,7 +5,7 @@ class Student(BaseModel):
     name:str 
     age: Optional[int] = None
     email: EmailStr
-    cgpa : float =  Field(gt=0, lt=4.0, default=0.0, description="CGPA must be between 0.0 and 4.0") 
+    cgpa : float =  Field(ge=0, lt=4.0, default=0.0, description="CGPA must be between 0.0 and 4.0") 
 
 
 newStudent = Student(name="Aryan", email="aryan@gmail.com", cgpa=3.5)
--- structured_output/typedDict_demo.py
+++ structured_output/typedDict_demo.py
@@ -7,7 +7,7 @@ class User(TypedDict):
 
 user1:User = {
     "name":"Aryan",
-    "age": "22"
+    "age": "22" # This will still work because TypedDict does not enforce type checking at runtime, but it will raise a type checker warning since 'age' is expected to be an int, not a str.
 }
 
 print(user1)
